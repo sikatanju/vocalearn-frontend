ffab99f7bd746b9c77f607321ba1cdd262859323
sikatanju <saadtanju@gmail.com>
2025-07-08 16:02:14 +0600

    added dynamic backend url

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:8000/vocalearn",
+    baseURL: import.meta.env.VITE_VOCALEARN_URL,
     headers: {
         "Content-Type": "application/json",
     },
